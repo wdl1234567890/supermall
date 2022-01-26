adf744f51816c03905a76ae4363b18c82b6fe938
FuLi <53114138+wdl1234567890@users.noreply.github.com>
2022-01-26 13:21:44 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -8,7 +8,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: "http://152.136.185.210:7878/api/hy66",
+	baseURL: "http://**",
 	timeout: 500
 })
 instance.interceptors.response.use(res => {
