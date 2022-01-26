25467425c7d298ed2aa9ae8868d4a05fbd504c69
FL <1639913166@qq.com>
2022-01-26 12:56:57 +0800

    test

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,14 @@
+/*
+ * @Author: FL
+ * @Date: 2022-01-24 20:50:25
+ * @LastEditors: FL
+ * @LastEditTime: 2022-01-26 12:50:41
+ * @Description: 
+ */
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: "http://***",
+	baseURL: "http://152.136.185.210:7878/api/hy66",
 	timeout: 500
 })
 instance.interceptors.response.use(res => {
